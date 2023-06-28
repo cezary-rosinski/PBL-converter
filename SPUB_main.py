c44b14b183edee85f700b3f7283bf7a7391ffd5f
Cezary <cezary.rosinski@gmail.com>
2023-06-28 14:41:46 +0200

    uproszczenie main

--- SPUB_main.py
+++ SPUB_main.py
@@ -241,9 +241,7 @@ tree.write('./xml_output/import_journal_items_and_books.xml', encoding='UTF-8')
 
 
 #%% RETRO
-
-annotation_bib_rec = 'Rekord pochodzi z automatycznego parsowania drukowanego tomu PBL. Oryginalna postać rekordu w druku: {original_rec}.'
-annotation_auth_files = 'Rekord powstał wskutek w pełni automatycznego parsowania drukowanego tomu PBL.'     
+# przygotowuje pliki do manualnej selekcji rodzajów i działów
 
 # preparing headings
 for filename in tqdm(os.listdir('retro_input')[::-1]):
@@ -314,146 +312,9 @@ for filename in tqdm(os.listdir('retro_input')[::-1]):
 #             count_df.to_excel(f"./additional_files/retro_count_{today}.xlsx", index=False)
 
 # print('\nauth_len: ', count_df['auth_len'].sum(), '\nrecs_len: ',count_df['recs_len'].sum(), '\nfull_len: ', count_df['full_len'].sum())
-    
-#%% load input
-filename = '1988_t1'
-year = '1988'
-with open(f"./retro_input/{filename}.json", encoding='utf8') as f:
-    retro_data = json.load(f)
-
-#%%% preprocess retro
-annotation_bib_rec = 'Rekord pochodzi z automatycznego parsowania drukowanego tomu PBL. Oryginalna postać rekordu w druku: {original_rec}.'
-annotation_auth_files = 'Rekord powstał wskutek w pełni automatycznego parsowania drukowanego tomu PBL.'
-
-# authorities lists
-retro_pre_persons, retro_pre_places, retro_pre_journals, retro_pre_institutions = get_retro_authorities_sets(retro_data, filename)
-
-retro_places = [Place(id_='', lat='', lon='', name=e, annotation=annotation_auth_files) for e in tqdm(retro_pre_places)]
-last_number = give_fake_id(retro_places)
-
-retro_persons = [Person(id_='', viaf='', name=e, annotation=annotation_auth_files) for e in tqdm(retro_pre_persons)]
-last_number = give_fake_id(retro_persons, last_number)
-
-retro_institutions = [Institution(id_='', viaf='', name=e, annotation=annotation_auth_files) for e in tqdm(retro_pre_institutions)]
-last_number = give_fake_id(retro_institutions, last_number)
-
-retro_journals = [Journal(title=e[0], years_with_numbers_set=((year, e[1]),), annotation=annotation_auth_files) for e in tqdm(retro_pre_journals)]
-last_number = give_fake_id(retro_journals, last_number)
-
-# # --------------------------------------
-
-# prep biblio records
-records_prep = preprocess_retro(retro_data, filename, year)
-without_title = [e for e in records_prep if not 'rec_title' in e]
-if without_title:
-    raise Exception('without_title not empty!')
-
-# ----------------------------------
-retro_persons_to_connect = {}
-for p in retro_persons:
-    for name in p.names:
-        retro_persons_to_connect.update({name.value: p.id})
-
-retro_journals_to_connect = {}
-for j in retro_journals:
-    for title in j.titles:
-        retro_journals_to_connect.update({title.value: j})
-
-retro_institutions_to_connect = {}
-for i in retro_institutions:
-    for name in i.names:
-        retro_institutions_to_connect.update({name.value: i.id})
-
-retro_books = [Book.from_retro(e) for e in tqdm(records_prep) if e['rec_type']=='KS']
-last_number = give_fake_id(retro_books, last_number)
-
-for book in tqdm(retro_books):
-    book.connect_with_persons(retro_persons_to_connect)
-    book.connect_publisher(retro_places, retro_institutions_to_connect)
-
-retro_journal_items = [JournalItem.from_retro(e) for e in tqdm(records_prep) if e['rec_type']=='ART']
-last_number = give_fake_id(retro_journal_items, last_number)
-
-for journal_item in tqdm(retro_journal_items):
-    journal_item.connect_with_persons(retro_persons_to_connect)
-    journal_item.connect_with_journals(retro_journals_to_connect)
-
-#%% retro xml creation
-# places
-places_xml = ET.Element('pbl')
-files_node = ET.SubElement(places_xml, 'files')
-places_node = ET.SubElement(files_node, 'places')
-for place in tqdm(retro_places):
-    places_node.append(place.to_xml())
-
-tree = ET.ElementTree(places_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_places_{filename}.xml', encoding='UTF-8')
-
-# persons
-persons_xml = ET.Element('pbl')
-files_node = ET.SubElement(persons_xml, 'files')
-people_node = ET.SubElement(files_node, 'people')
-for person in tqdm(retro_persons):
-    people_node.append(person.to_xml())
-
-tree = ET.ElementTree(persons_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_people_{filename}.xml', encoding='UTF-8')
-
-# institutions
-institutions_xml = ET.Element('pbl')
-files_node = ET.SubElement(institutions_xml, 'files')
-institutions_node = ET.SubElement(files_node, 'institutions')
-for institution in tqdm(retro_institutions):
-    institutions_node.append(institution.to_xml())
-
-tree = ET.ElementTree(institutions_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_institutions_{filename}.xml', encoding='UTF-8')
-
-# journals
-journals_xml = ET.Element('pbl')
-files_node = ET.SubElement(journals_xml, 'files')
-journals_node = ET.SubElement(files_node, 'journals')
-journals_years_node = ET.SubElement(files_node, 'journal-years')
-journals_numbers_node = ET.SubElement(files_node, 'journal-numbers')
-for journal in tqdm(retro_journals):
-    journals_node.append(journal.to_xml())
-    for year_xml in journal.years_to_xml():
-        journals_years_node.append(year_xml)
-    for number_xml in journal.numbers_to_xml():
-        journals_numbers_node.append(number_xml)
-  
-tree = ET.ElementTree(journals_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_journals_{filename}.xml', encoding='UTF-8')
-
-# journal items
-records_xml = ET.Element('pbl')
-files_node = ET.SubElement(records_xml, 'records')
-journal_items_node = ET.SubElement(files_node, 'journal-items')
-for journal_item in tqdm(retro_journal_items):
-    journal_items_node.append(journal_item.to_xml())
-
-tree = ET.ElementTree(records_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_journal_items_{filename}.xml', encoding='UTF-8')
-
-# books
-records_xml = ET.Element('pbl')
-files_node = ET.SubElement(records_xml, 'records')
-books_node = ET.SubElement(files_node, 'books')
-for book in tqdm(retro_books):
-    books_node.append(book.to_xml())
-    
-tree = ET.ElementTree(records_xml)
-ET.indent(tree, space="\t", level=0)
-tree.write(f'./xml_output/import_retro_books_{filename}.xml', encoding='UTF-8')
 
 #%% retro loop
-
-annotation_bib_rec = 'Rekord pochodzi z automatycznego parsowania drukowanego tomu PBL. Oryginalna postać rekordu w druku: {original_rec}.'
+# przetwarza wszystkie pliki z retro, jeśli trzeba przetworzyć jeden plik, wystarczy podać jednoelementową listę do pętli
 annotation_auth_files = 'Rekord powstał wskutek w pełni automatycznego parsowania drukowanego tomu PBL.'
 
 for file in tqdm(os.listdir('retro_input')[::-1]):
